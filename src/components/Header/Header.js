18201f484ee83ae5b9d7bae54809056ecfa8faf0
dude lao <brycelipscomb@gmail.com>
2021-10-31 06:02:36 -0400

    updated the nav button names to better reflect the content they will redirect to

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,10 +5,10 @@ export default function Header(){
         <header>
             <nav>
                 <div>Logo</div>
-                <div>div</div>
-                <div>div</div>
-                <div>div</div>
-                <div>div</div>
+                <div>About</div>
+                <div>Champions</div>
+                <div>Blog</div>
+                <div>OP.GG</div>
             </nav>
         </header>
     )
