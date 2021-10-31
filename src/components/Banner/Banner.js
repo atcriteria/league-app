eec5803d987a60e3a2f6153c99d9d85f566b6702
dude lao <brycelipscomb@gmail.com>
2021-10-31 05:53:21 -0400

    more misc styling to tweak the appearance of the banner. I'm going to have to pay close attention to breakpoints when I make this responsive

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -5,8 +5,9 @@ export default function Banner(){
     return(
         <aside>
             <div className="fade-wrapper"></div>
-            <div className="text-wrapper">Greetings, Summoner</div>
+            <div className="text-wrapper"><span>AtCriteria</span></div>
             <div className="img-container" alt="banner" style={{backgroundImage:`url(${origin+"/images/Ezreal_6.jpg"})`}} />
+            <img src="https://universe.leagueoflegends.com/images/t1HeaderDivider.png" alt="section divider"/>
         </aside>
     )
 }
