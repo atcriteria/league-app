72d3401396feb3b54147ca032be8080860082b8e
dude lao <brycelipscomb@gmail.com>
2021-10-31 04:17:52 -0400

    adjusted some of the default styles, implementing the header and main nav bar

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import './header.css';
+
+export default function Header(){
+    return(
+        <header>
+            <nav>
+                <div>Logo</div>
+                <div>div</div>
+                <div>div</div>
+                <div>div</div>
+                <div>div</div>
+            </nav>
+        </header>
+    )
+}
