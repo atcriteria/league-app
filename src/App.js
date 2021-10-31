38e57abb2058be943073e6ed37fce1da55803aab
dude lao <brycelipscomb@gmail.com>
2021-10-31 19:59:01 -0400

    About section init

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Banner from './components/Banner/Banner';
+import About from './components/About/About';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Banner />
-      App
+      <About />
     </div>
   );
 }
