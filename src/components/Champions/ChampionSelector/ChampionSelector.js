b84c8d6e1db0a90ed2736c25dbf228577964f1bc
dude lao <brycelipscomb@gmail.com>
2021-11-01 15:23:50 -0400

    refactoring some of the ChampionSelector styles to be more in keeping with the original style

--- src/components/Champions/ChampionSelector/ChampionSelector.js
+++ src/components/Champions/ChampionSelector/ChampionSelector.js
@@ -1,15 +1,21 @@
+import './championSelector.css';
 import ChampionButton from "../ChampionButton";
 
 export default function ChampionSelector({champions, selectedChampion, selectNewChampion}){
     return(
         <div className="champion-select">
-            {champions.map((champion, ind) => {
-                if (champion === selectedChampion){
-                    return "";
-                } else {
-                    return <ChampionButton selectNewChampion={selectNewChampion} champion={champion} key={ind} id={ind} />
-                }
-            })}
+            <div className="champion-select-header">
+                Champions
+            </div>
+            <div className="champion-select-buttons">
+                {champions.map((champion, ind) => {
+                    if (champion === selectedChampion){
+                        return "";
+                    } else {
+                        return <ChampionButton selectNewChampion={selectNewChampion} champion={champion} key={ind} id={ind} />
+                    }
+                })}
+            </div>
         </div>
     )
 }
