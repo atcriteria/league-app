ece14f036df3824ac08a589f84765c43da3b0235
dude lao <brycelipscomb@gmail.com>
2021-11-01 05:03:23 -0400

    added functionality to change between selected champions, rendered champion avatar to the main avatar slot

--- src/components/Champions/ChampionButton.js
+++ src/components/Champions/ChampionButton.js
@@ -1,10 +1,15 @@
 import "./championButton.css";
 
 export default function ChampionButton(props){
-    const {champion} = props;
+    const {champion, selectNewChampion, id} = props;
+
+    const handleClick = e => {
+        selectNewChampion(id);
+    }
+
     return(
         <div className="champion-button-container" >
-            <div className="champion-button-wrapper">
+            <div onClick={handleClick} className="champion-button-wrapper">
                 <div className="champion-button-image-wrapper" style={{backgroundImage: `url(${champion.image})`}} />
             </div>
             <div>{champion.name}</div>
--- src/components/Champions/Champions.js
+++ src/components/Champions/Champions.js
@@ -4,18 +4,22 @@ import championData from './championData';
 import ChampionButton from './ChampionButton';
 
 export default function Champions(){
-    const [selectedChampion, seteSelectedChampion] = useState(championData[0]);
+    const [selectedChampion, setSelectedChampion] = useState(championData[0]);
     const [champions, setChampions] = useState(championData);
+
+    const selectNewChampion = (id) => {
+        return setSelectedChampion(championData[id])
+    }
 
     return(
         <section className="champions-section">
             <div className="champion-select">
                 {champions.map((champion, ind) => {
-                    return <ChampionButton champion={champion} key={ind} />
+                    return <ChampionButton selectNewChampion={selectNewChampion} champion={champion} key={ind} id={ind} />
                 })}
             </div>
             <div className="champion-info">
-                <div className="champion-avatar"></div>
+                <div className="champion-avatar" style={{backgroundImage: `url(${selectedChampion.image})`}}></div>
                 <div className="champ-description"></div>
                 <div className="champ-metrics"></div>
             </div>
