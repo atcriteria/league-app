b8207f99648ebe107e6e123c125385035ebfd6cb
dude lao <brycelipscomb@gmail.com>
2021-10-31 21:50:06 -0400

    more work on the about section and styling. I really want to get the chamferred corners working in a relatively easy manner but it looks like I might have to get into canvases or SVGs to do so rather than simple CSS borders. I'll revisit this later once the app is further along

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,13 @@
 import './about.css';
+const origin = window.location.origin;
 
 export default function About(){
     return(
-        <section>
-            About
+        <section className="about-section">
+            <img src={`${origin}/Images/Gangplank_14.jpg`} alt="Pool Party Gragas representing AtCriteria" />
+            <article>
+                Quis ea sunt culpa aute irure mollit ad enim eu. Velit nisi laboris amet esse deserunt do laboris. Velit laboris in minim nostrud non cupidatat aliquip fugiat labore labore incididunt nisi pariatur dolor. Sunt amet incididunt laborum nostrud cillum. Quis ea sunt culpa aute irure mollit ad enim eu. Velit nisi laboris amet esse deserunt do laboris. Velit laboris in minim nostrud non cupidatat aliquip fugiat labore labore incididunt nisi pariatur dolor. Sunt amet incididunt laborum nostrud cillum.
+            </article>
         </section>
     )
 }
