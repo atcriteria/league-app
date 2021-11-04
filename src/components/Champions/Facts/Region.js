51cf2454d8ac53118ffe872c35e4c6fdd0d252ce
dude lao <brycelipscomb@gmail.com>
2021-11-04 04:39:08 -0400

    added region title and emblem area for the region section

--- src/components/Champions/Facts/Region.js
+++ src/components/Champions/Facts/Region.js
@@ -1,7 +1,16 @@
 export default function Region({region}){
     return(
         <div className="champion-region-wrapper">
-            Region
+            <div className="champion-region-content-wrapper">
+
+            </div>
+            <div className="champion-region-title-wrapper" >
+                <div className="champion-region-title">
+                    <p>Region</p>
+                    <p>{region}</p>
+                </div>
+                <div className="champion-region-emblem" />
+            </div>
         </div>
     )
 }
