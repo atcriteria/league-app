214e34af64fbe520d33c29ffbcbf31d5ddcce606
dude lao <brycelipscomb@gmail.com>
2021-11-06 03:25:53 -0400

    wrote a helper function for resolving passed region names into formattable names for image paths. Future goals are to rename these functions to be clearer as to what their purpose is as well as to do some photoshop magic to vastly reduce the sizes of the splash arts before crunching some image files into smaller files. The app is pretty fast locally but in a live setting I imagine downloading these large files would cause quite the wait time.

---
+++ src/components/Champions/Facts/resolveRegion.js
@@ -0,0 +1,20 @@
+// Helper function for resolving the image path to a regions
+// splash art given a specific champion object. 
+// Champion objects store their region name in object.region
+// and usually as a string, which is fine for simple names like
+// "Demacia" but not so much for places such as "Bandle City".
+// Image files use underscores in place of spaces, so the image
+// would be "bandle_city", not "Bandle City". This function 
+// resolves this.
+
+export default function resolveRegion(region){
+    // Make a copy of the string in all lowercase
+    let str = region.toLowerCase();
+
+    // split the name at the spaces and join the elements of
+    // the resulting array with an underscore
+    // "bandle city" => ["bandle", "city"] -> "bandle_city"
+    str.split(" ").join("_");
+
+    return str;
+}
