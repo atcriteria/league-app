3f5671e7891613d7c6d164eec8c6340663bc0e9f
dude lao <brycelipscomb@gmail.com>
2021-11-05 20:03:36 -0400

    fixed image names, added linking to images in the region component

--- src/components/Champions/Facts/Region.js
+++ src/components/Champions/Facts/Region.js
@@ -1,4 +1,7 @@
 export default function Region({region}){
+    const origin = window.location.origin;
+    let url = `${origin}/Images/factions/${region.toLowerCase()}_crest_icon.png`
+    console.log(url)
     return(
         <div className="champion-region-wrapper">
             <div className="champion-region-content-wrapper">
@@ -9,7 +12,9 @@ export default function Region({region}){
                     <p>Region</p>
                     <p>{region}</p>
                 </div>
-                <div className="champion-region-emblem" />
+                <div className="champion-region-emblem" >
+                    <img src={url} alt={`${region} crest icon`} />
+                </div>
             </div>
         </div>
     )
