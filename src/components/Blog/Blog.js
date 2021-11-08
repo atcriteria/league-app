74ff8c2a38c326993894e55f29687f39209c2add
dude lao <brycelipscomb@gmail.com>
2021-11-07 19:48:07 -0500

    added alignment buttons to the blog area

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -3,7 +3,36 @@ import './blog.css';
 export default function Blog(){
     return(
         <section className="blog-wrapper">
-            blah blah
+            <div className="align-buttons-wrapper" >
+                <div className="align-button" >
+                    <p>___________</p>
+                    <p>______</p>
+                    <p>_________</p>
+                    <p>_____</p>
+                </div>
+                <div className="align-button" >
+                    <p>___________</p>
+                    <p>______</p>
+                    <p>_________</p>
+                    <p>_____</p>
+                </div>
+                <div className="align-button" >
+                    <p>___________</p>
+                    <p>______</p>
+                    <p>_________</p>
+                    <p>_____</p>
+                </div>
+                <div className="align-button" >
+                    <p>_______  ___</p>
+                    <p>_   ________</p>
+                    <p>__  ____   __</p>
+                    <p>____   _____</p>
+                </div>
+            </div>
+            <div className="blog-content">
+                <h1>Title</h1>
+                <p>Blog Content goes here - blah blah blah blah blah blah blah blah</p>
+            </div>
         </section>
     )
 }
