8184db4ed403d8b5d16289371e2b923b0b3ed439
dude lao <brycelipscomb@gmail.com>
2021-11-07 19:53:35 -0500

    added event handler to listen for clicking the buttons as well as some styling to ignore clicking the divs within

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,28 +1,31 @@
 import './blog.css';
 
 export default function Blog(){
+    const handleClick = e => {
+        console.log(e.target.title)
+    }
     return(
         <section className="blog-wrapper">
             <div className="align-buttons-wrapper" >
-                <div className="align-button" >
+                <div onClick={handleClick} title="left" className="align-button" >
                     <p>___________</p>
                     <p>______</p>
                     <p>_________</p>
                     <p>_____</p>
                 </div>
-                <div className="align-button" >
+                <div onClick={handleClick} title="center" className="align-button" >
                     <p>___________</p>
                     <p>______</p>
                     <p>_________</p>
                     <p>_____</p>
                 </div>
-                <div className="align-button" >
+                <div onClick={handleClick} title="right" className="align-button" >
                     <p>___________</p>
                     <p>______</p>
                     <p>_________</p>
                     <p>_____</p>
                 </div>
-                <div className="align-button" >
+                <div onClick={handleClick} title="justify" className="align-button" >
                     <p>_______  ___</p>
                     <p>_   ________</p>
                     <p>__  ____   __</p>
