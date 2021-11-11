ffa8cc69133d7c5dc7a14c0effb2d40b64bab438
dude lao <brycelipscomb@gmail.com>
2021-11-11 04:12:42 -0500

    added testid

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,7 +3,7 @@ const origin = window.location.origin;
 
 export default function About(){
     return(
-        <section className="about-section">
+        <section className="about-section" data-testid="about-testID">
             <img src={`${origin}/Images/Gangplank_14.jpg`} alt="Pool Party Gragas representing AtCriteria" />
             <article>
                 Quis ea sunt culpa aute irure mollit ad enim eu. Velit nisi laboris amet esse deserunt do laboris. Velit laboris in minim nostrud non cupidatat aliquip fugiat labore labore incididunt nisi pariatur dolor. Sunt amet incididunt laborum nostrud cillum. Quis ea sunt culpa aute irure mollit ad enim eu. Velit nisi laboris amet esse deserunt do laboris. Velit laboris in minim nostrud non cupidatat aliquip fugiat labore labore incididunt nisi pariatur dolor. Sunt amet incididunt laborum nostrud cillum.
