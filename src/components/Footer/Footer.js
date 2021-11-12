92e6363f175c21c5052ba9245776dd0021a5870c
dude lao <brycelipscomb@gmail.com>
2021-11-12 18:59:17 -0500

    updated Footer elements to be more semantic

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,18 @@
 import './footer.css';
 
 export default function Footer(){
+    const origin = window.location.origin;
+
     return(
         <footer className="footer-wrapper">
-            <div className="footer-img" >
-                Picture
-            </div>
-            <div className="footer-content">
-                Links
-            </div>
+            <div className="footer-img" style={{backgroundImage: `url(${origin}/Images/footer-image.jpg)`}} />
+            <nav className="footer-content">
+                <a href="localhost:3000/#">Link</a>
+                <a href="localhost:3000/#">Link</a>
+                <a href="localhost:3000/#">Link</a>
+                <a href="localhost:3000/#">Link</a>
+                <a href="localhost:3000/#">Link</a>
+            </nav>
         </footer>
     )
 }
