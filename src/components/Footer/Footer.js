246246aa5e774779834762814218049f9e3cdbf6
dude lao <brycelipscomb@gmail.com>
2021-11-12 18:45:55 -0500

    initializing the Footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './footer.css';
+
+export default function Footer(){
+    return(
+        <footer className="footer-wrapper">
+            <div className="footer-img" >
+                Picture
+            </div>
+            <div className="footer-content">
+                Links
+            </div>
+        </footer>
+    )
+}
