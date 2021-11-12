4730d78822ecdb0616b80dabd0da8f8008f7a8d1
dude lao <brycelipscomb@gmail.com>
2021-11-12 18:46:25 -0500

    Adding Footer to the App render

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Banner from './components/Banner/Banner';
 import About from './components/About/About';
 import Champions from './components/Champions/Champions';
 import Blog from './components/Blog/Blog';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Champions />
       <PageBreak title={"Blog"}/>
       <Blog />
+      <Footer />
     </div>
   );
 }
