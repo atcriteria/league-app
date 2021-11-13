398a05d7da8dbd5a98e56a2be8b605e614ccf474
dude lao <brycelipscomb@gmail.com>
2021-11-12 20:32:25 -0500

    added some id's and a link, updated scroll behavior for the app to smooth for a smoother scrolling experience

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ export default function Header(){
         <header className="app-header" name="main-header" data-testid="header-testID" >
             <nav>
                 <div>Logo</div>
-                <div>About</div>
+                <div><a href="#banner-section" >About</a></div>
                 <div><a href="#champion-section">Champions</a></div>
                 <div>Blog</div>
                 <div><a href="https://na.op.gg/summoner/userName=AtCriteria" target="_blank" rel="noreferrer" >OP.GG</a></div>
